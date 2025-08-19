0b0ed9d3335b2cc7a8b6308f841ec708d4db93fa
Varshith Goud <varshithgoudbalagouni18@gmail.com>
2025-08-19 10:54:07 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const taskController = require('./taskController');
+const taskController = require('../controllers/taskController');
 const protect = require('./authMiddleware');
 
 router.use(protect);
@@ -12,4 +12,5 @@ router.delete('/:id', taskController.deleteTask);
 
 
 module.exports = router;
+
 
