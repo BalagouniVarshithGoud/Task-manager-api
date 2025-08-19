4e6edc12a1280dad5143d4431c46f54e79c809a3
Varshith Goud <varshithgoudbalagouni18@gmail.com>
2025-08-19 11:31:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const app = express();
 app.use(express.json());
 
 // Route files
-app.use("/api/auth", require("../routes/authRoutes"));   // ✅ will crash if auth.js is broken
-app.use("/api/tasks", require("../routes/taskRoutes"));  // ✅ optional if task.js is ready
+app.use("/api/auth", require("./routes/authRoutes"));   // ✅ will crash if auth.js is broken
+app.use("/api/tasks", require("./routes/taskRoutes"));  // ✅ optional if task.js is ready
 
 app.get("/health", (req, res) => {
   res.send("API is healthy ✅");
 });
 
 module.exports = app;
+
 
 
