d7da7c2aead10b042fb3372db21720a403143c17
Varshith Goud <varshithgoudbalagouni18@gmail.com>
2025-08-19 10:53:40 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("./authController");
+const authController = require("../controllers/authController");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
 module.exports = router;
+
 
